a93893be2563146ebbb94656a7dab4e403774d3a
Pipat Saengow <pay2630@gmail.com>
2022-12-08 23:18:53 +0700

    webpack setup

---
+++ src/index.js
@@ -0,0 +1,3 @@
+const ele = document.createElement("p")
+ele.innerText = "Hello World"
+document.body.appendChild(ele)
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: "RCG Final Project"
+    })
+  ],
+  mode: "development",
+  devtool: 'inline-source-map',
+};
