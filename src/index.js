909b9e71919b6d682d783c33bd84d7fa3740f4d4
Pipat Saengow <pay2630@gmail.com>
2022-12-09 03:39:12 +0700

    basic effect

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import "./style.css";
 import testShaderCode from "raw-loader!./test.glsl";
+import { PlaneGeometry, Scene } from 'three';
 
 // Setup Scene and Renderer
 const scene = new THREE.Scene();
@@ -11,29 +12,55 @@ camera.position.z = 2;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
 
-const geometry = new THREE.BufferGeometry();
-geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array([
-    //2 Triangle
-    -width/2, -height/2,  1.0,
-	 width/2, -height/2,  1.0,
-	 width/2,  height/2,  1.0,
+const geometry = new THREE.PlaneGeometry(width, height);
 
-	 width/2,  height/2,  1.0,
-	-width/2,  height/2,  1.0,
-	-width/2, -height/2,  1.0,
-    ]),
-    3)
-)
+
+const testTex = new THREE.TextureLoader().load("test.jpg")
+let bufferA = new THREE.WebGLRenderTarget(width, height, {minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter});
+let bufferB = new THREE.WebGLRenderTarget(width, height, {minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter});
+
+const testMaterial = new THREE.MeshBasicMaterial({color: 0xFF0000});
 const material = new THREE.ShaderMaterial({
-    fragmentShader: testShaderCode
+    fragmentShader: testShaderCode,
+    uniforms: {
+        previous: {
+            type: "t",
+            value: bufferA.texture,
+        },
+        screenSize: {
+            type: 'v2',
+            value: new THREE.Vector2(width, height)
+        }
+    }
 })
 const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const bufferScene =  new Scene()
+bufferScene.add(mesh);
+
+//const displayMaterial = new THREE.MeshBasicMaterial({map: texA});
+const displayMaterial = new THREE.MeshBasicMaterial({map: bufferA.texture});
+const displayMesh = new THREE.Mesh(geometry, displayMaterial);
+scene.add(displayMesh);
+
+let f = false;
 
 // Render Loop
 function animate() {
+    [bufferA, bufferB] = [bufferB, bufferA];
+    material.uniforms.previous.value = bufferA.texture;
+    renderer.setRenderTarget(bufferB);
+    renderer.render(bufferScene, camera);
+
+    renderer.setRenderTarget(null);
+    displayMesh.material.map = bufferB.texture;
+    renderer.render(scene, camera)
+
+    if (!f) {
+        console.log(renderer.info);
+        console.log({width, height});
+        f = true;
+    }
     requestAnimationFrame(animate);
-    renderer.render(scene, camera);
 }
 
 // DOM Insertion and Start Render Loop
--- src/test.glsl
+++ src/test.glsl
@@ -1,3 +1,20 @@
+uniform sampler2D previous;
+uniform vec2 screenSize;
+
 void main() {
-    gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+    vec2 relCord = gl_FragCoord.xy / screenSize.xy;
+    vec2 unitDist = vec2(1.0, 1.0)/screenSize.xy;
+
+    vec4 left = texture2D(previous, vec2(relCord.x - unitDist.x, relCord.y)),
+        right = texture2D(previous, vec2(relCord.x + unitDist.x, relCord.y)),
+        up = texture2D(previous, vec2(relCord.x , relCord.y + unitDist.y)),
+        down = texture2D(previous, vec2(relCord.x , relCord.y - unitDist.y)),
+        cen = texture2D(previous, relCord);
+    
+    gl_FragColor = cen;
+    gl_FragColor.r += 0.254*(left.r + right.r + up.r + down.r - 4.0*cen.r);
+
+    if (distance(gl_FragCoord.xy, screenSize.xy/2.0) < 15.0) {
+        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    }
 }
